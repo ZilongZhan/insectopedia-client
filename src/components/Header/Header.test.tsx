f75f1fdac557c658521c00b0476d723aa4c4e826
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-07 16:46:54 +0200

    Add Header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,27 @@
+import { render } from "vitest-browser-react";
+import { page } from "@vitest/browser/context";
+import Header from "./Header";
+
+describe("Given the Header component", () => {
+  describe("When it renders", () => {
+    test("Then it should show 'Insectopedia' inside a level 1 heading", () => {
+      const expectedTitle = /insectopedia/i;
+
+      render(<Header />);
+
+      const appTitle = page.getByRole("heading", { name: expectedTitle });
+
+      expect(appTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show a monarch butterfly resting on a flower", () => {
+      const expectedAltText = /a monarch butterfly resting on a flower/i;
+
+      render(<Header />);
+
+      const banner = page.getByAltText(expectedAltText);
+
+      expect(banner).toBeInTheDocument();
+    });
+  });
+});
