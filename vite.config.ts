a52d87c260052ff477b9db76c3a625b248104c41
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-07 16:42:39 +0200

    Setup vitest coverage & browser mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    reporters: "verbose",
+    coverage: {
+      provider: "v8",
+      include: ["**/*.tsx", "**/*.ts"],
+      exclude: [
+        "src/main.tsx",
+        "**/*.d.ts",
+        "**/types.ts",
+        "**/App.tsx",
+        "**/*.config.*",
+      ],
+      reportsDirectory: "coverage",
+    },
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      headless: true,
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
