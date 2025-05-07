b08c70116712c20f5f3955bd7eb7c5a1c88b371b
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-07 16:49:30 +0200

    Render Layout in App component & remove dead code

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./components";
+import App from "./components/App/App";
 
+import "@fontsource/noto-sans-thai-looped/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
