a242e8cc9fd08d4d355c7a59746b13ebca8bf6dd
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-12 18:08:47 +0200

    Add getBugsInfo method to BugsClient

---
+++ src/bug/client/BugsClient.ts
@@ -0,0 +1,29 @@
+import { mapBugsDtoToBugs } from "../dto/mappers";
+import type { BugsInfoDto } from "../dto/types";
+import type { BugsInfo } from "../types";
+import type { BugsClientStructure } from "./types";
+
+class BugsClient implements BugsClientStructure {
+  private readonly apiUrl = import.meta.env.VITE_API_URL;
+
+  public async getBugsInfo(pageNumber: number): Promise<BugsInfo> {
+    const response = await fetch(
+      `${this.apiUrl}/bugs?pageNumber=${pageNumber}`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Error fetching bugs info");
+    }
+
+    const { bugs: bugsDto, bugsTotal } = (await response.json()) as BugsInfoDto;
+
+    const bugs = mapBugsDtoToBugs(bugsDto);
+
+    return {
+      bugs,
+      bugsTotal,
+    };
+  }
+}
+
+export default BugsClient;
---
+++ src/bug/client/types.ts
@@ -0,0 +1,5 @@
+import type { BugsInfo } from "../types";
+
+export interface BugsClientStructure {
+  getBugsInfo: (pageNumber: number) => Promise<BugsInfo>;
+}
