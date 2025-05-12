cf5df9f6f8ab8389068bbd4c1f76c5fcf1e11f73
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-12 02:10:46 +0200

    Add StarSvg component

---
+++ src/components/shared/StarSvg/StarSvg.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "vitest-browser-react";
+import StarSvg from "./StarSvg";
+import { page } from "@vitest/browser/context";
+
+describe("Given the StarSvg component", () => {
+  describe("When it renders", () => {
+    test("Then it should show icon of a 5 pointed star", () => {
+      render(<StarSvg />);
+
+      const svgElement = page.getByRole("img", {
+        name: /icon of a 5 pointed star/i,
+      });
+
+      expect(svgElement).toBeInTheDocument();
+    });
+  });
+});
