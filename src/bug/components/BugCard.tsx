fd3e8ab553d781537509dbbb7e2e6a3b6105a743
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-12 02:11:38 +0200

    Add BugCard component

---
+++ src/bug/components/BugCard.tsx
@@ -0,0 +1,38 @@
+import StarSvg from "../../components/shared/StarSvg/StarSvg";
+import type { Bug } from "../types";
+
+import "./BugCard.css";
+
+interface BugCardProps {
+  bug: Bug;
+}
+const BugCard: React.FC<BugCardProps> = ({
+  bug: { imageUrl, imageAlt, isFavorite, name, scientificName },
+}) => {
+  const modifier = isFavorite ? " favorite-icon--true" : "";
+
+  return (
+    <article className="bug">
+      <img
+        className="bug__image"
+        src={imageUrl}
+        alt={imageAlt}
+        height={115}
+        width={75}
+      />
+      <div className="info-container info-container--card">
+        <h3 className="bug__name">{name}</h3>
+        <i className="bug__latin-name">{scientificName}</i>
+        <div className="buttons-container">
+          <StarSvg
+            className={`favorite-icon${modifier}`}
+            width={20}
+            height={19}
+          />
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default BugCard;
