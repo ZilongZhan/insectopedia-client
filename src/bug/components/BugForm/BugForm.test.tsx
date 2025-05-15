73b2f51aa964ab3077d2fba3a654a5997cdf636a
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-15 20:30:45 +0200

    Add BugForm component

---
+++ src/bug/components/BugForm/BugForm.test.tsx
@@ -0,0 +1,200 @@
+import { render } from "vitest-browser-react";
+import { page, userEvent } from "@vitest/browser/context";
+import BugForm from "./BugForm";
+
+describe("Given the BugForm component", () => {
+  const user = userEvent.setup();
+
+  describe("When it renders", () => {
+    test("Then it should show a 'Common name' input", () => {
+      render(<BugForm />);
+
+      const nameInput = page.getByLabelText(/common name/i);
+
+      expect(nameInput).toBeInTheDocument();
+    });
+
+    test("Then it should show a disabled 'Send report' button", () => {
+      render(<BugForm />);
+
+      const submitButton = page.getByRole("button", { name: /send report/i });
+
+      expect(submitButton).toBeInTheDocument();
+      expect(submitButton).toBeDisabled();
+    });
+
+    test("Then it should show a 'Add to favorites' input", () => {
+      render(<BugForm />);
+
+      const isFavoriteInput = page.getByLabelText(/add to favorites/i);
+
+      expect(isFavoriteInput).toBeInTheDocument();
+    });
+
+    test("Then it should show a 'Classification' form section with phylum, class, and order inputs", () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const classInput = classificationSection.getByRole("combobox", {
+        name: /class/i,
+      });
+      const phylumInput = classificationSection.getByRole("combobox", {
+        name: /phylum/i,
+      });
+      const orderInput = classificationSection.getByRole("combobox", {
+        name: /order/i,
+      });
+
+      expect(classificationSection).toBeInTheDocument();
+      expect(phylumInput).toBeInTheDocument();
+      expect(classInput).toBeInTheDocument();
+      expect(orderInput).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user types 'Hello World' in the 'Common name' input", () => {
+    test("Then it should show 'Hello World'", async () => {
+      const expectedInput = "Hello World";
+      const inputLabel = /common name/i;
+
+      render(<BugForm />);
+
+      const nameInput = page.getByLabelText(inputLabel);
+
+      await user.type(nameInput, expectedInput);
+
+      expect(nameInput).toHaveValue(expectedInput);
+    });
+  });
+
+  describe("When the user clicks 'Add to favorites' input", () => {
+    test("Then it should be checked", async () => {
+      const inputLabel = /add to favorites/i;
+
+      render(<BugForm />);
+
+      const isFavoriteInput = page.getByLabelText(inputLabel);
+
+      await user.click(isFavoriteInput);
+
+      expect(isFavoriteInput).toBeChecked();
+    });
+  });
+
+  describe("When the 'Arthropoda' option of the phylum input in 'Classification' section is selected", () => {
+    test("Then it the 'Arthropoda' option should be selected", async () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const phylumInput = classificationSection.getByRole("combobox", {
+        name: /phylum/i,
+      });
+
+      await phylumInput.selectOptions("Arthropoda");
+
+      expect(phylumInput).toHaveValue("Arthropoda");
+    });
+
+    test("Then the 'Insecta' option of the class input in 'Classification' section should be available", async () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const phylumInput = classificationSection.getByRole("combobox", {
+        name: /phylum/i,
+      });
+
+      await phylumInput.selectOptions("Arthropoda");
+
+      const classInput = classificationSection.getByRole("combobox", {
+        name: /class/i,
+      });
+
+      const insectaOption = classInput.getByRole("option", {
+        name: /insecta/i,
+      });
+
+      expect(insectaOption).toBeInTheDocument();
+    });
+  });
+
+  describe("When the 'Insecta' option of the class input in 'Classification' section is selected", () => {
+    test("Then the 'Coleoptera' option of the order input in 'Classification' section should be available", async () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const phylumInput = classificationSection.getByRole("combobox", {
+        name: /phylum/i,
+      });
+
+      await phylumInput.selectOptions("Arthropoda");
+
+      const classInput = classificationSection.getByRole("combobox", {
+        name: /class/i,
+      });
+
+      await classInput.selectOptions("Insecta");
+
+      const orderInput = classificationSection.getByRole("combobox", {
+        name: /order/i,
+      });
+
+      const coleopteraOption = orderInput.getByRole("option", {
+        name: /coleoptera/i,
+      });
+
+      expect(coleopteraOption).toBeInTheDocument();
+    });
+  });
+
+  describe("When no option of the phylum input in 'Classification' section is selected", () => {
+    test("Then the 'Insecta' option of the class input in 'Classification' section should not be available", async () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const classInput = classificationSection.getByRole("combobox", {
+        name: /class/i,
+      });
+
+      const insectaOption = classInput.getByRole("option", {
+        name: /insecta/i,
+      });
+
+      await expect.element(insectaOption).not.toBeInTheDocument();
+    });
+  });
+
+  describe("When no option of the class input in 'Classification' section is selected", () => {
+    test("Then the 'Coleoptera' option of the order intpu in 'Classification' section should not be available", async () => {
+      render(<BugForm />);
+
+      const classificationSection = page.getByRole("group", {
+        name: /classification/i,
+      });
+
+      const orderInput = classificationSection.getByRole("combobox", {
+        name: /order/i,
+      });
+
+      const coleopteraOption = orderInput.getByRole("option", {
+        name: /coleoptera/i,
+      });
+
+      await expect.element(coleopteraOption).not.toBeInTheDocument();
+    });
+  });
+});
---
+++ src/bug/components/BugForm/BugForm.tsx
@@ -0,0 +1,169 @@
+import { v4 as uuid } from "uuid";
+import classifications from "../../../data/classification";
+import useForm from "../../hooks/useForm/useForm";
+
+import "./BugForm.css";
+
+const BugForm: React.FC = () => {
+  const {
+    bugFormData: {
+      className,
+      description,
+      imageUrl,
+      isDangerous,
+      isFavorite,
+      name,
+      order,
+      phylum,
+      scientificName,
+    },
+    handleOnChange,
+    handleSubmit,
+    isValidData,
+    classOptions,
+    orderOptions,
+  } = useForm();
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form__group">
+        <label className="form__group__input-label" htmlFor="name">
+          Common name:
+        </label>
+        <input
+          className="form__group__input"
+          id="name"
+          type="text"
+          value={name}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+      <div className="form__group">
+        <label className="form__group__input-label" htmlFor="scientificName">
+          Latin name:
+        </label>
+        <input
+          className="form__group__input"
+          id="scientificName"
+          type="text"
+          value={scientificName}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+      <fieldset className="form__group">
+        <legend className="form__group__input-label form__group__input-label--legend">
+          Classification:
+        </legend>
+        <div className="classification">
+          <select
+            className="form__group__input"
+            aria-label="Taxonomic rank: phylum"
+            id="phylum"
+            value={phylum}
+            onChange={handleOnChange}
+            required
+          >
+            <option value="">Phylum -- not selected</option>
+            {classifications.map(({ name }) => (
+              <option key={uuid()} value={name}>
+                {name}
+              </option>
+            ))}
+            <option value="other">Other</option>
+          </select>
+          <select
+            className="form__group__input"
+            aria-label="Taxonomic rank: class"
+            id="className"
+            value={className}
+            onChange={handleOnChange}
+            required
+          >
+            <option value="">Class -- not selected</option>
+            {classOptions.map(({ name }) => (
+              <option key={uuid()} value={name}>
+                {name}
+              </option>
+            ))}
+            <option value="other">Other</option>
+          </select>
+          <select
+            className="form__group__input"
+            aria-label="Taxonomic rank: order"
+            id="order"
+            value={order}
+            onChange={handleOnChange}
+            required
+          >
+            <option value="">Order -- not selected</option>
+            {orderOptions.map((order) => (
+              <option key={uuid()} value={order}>
+                {order}
+              </option>
+            ))}
+            <option value="other">Other</option>
+          </select>
+        </div>
+      </fieldset>
+      <div className="form__group">
+        <label className="form__group__input-label" htmlFor="imageUrl">
+          Link to image:
+        </label>
+        <input
+          className="form__group__input"
+          id="imageUrl"
+          type="link"
+          value={imageUrl}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+      <div className="form__group">
+        <label className="form__group__input-label" htmlFor="description">
+          Description:
+        </label>
+        <textarea
+          className="form__group__input"
+          id="description"
+          rows={5}
+          value={description}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+      <div className="checkboxes-container">
+        <div className="form__group form__group--horizontal">
+          <input
+            className="form__group__input"
+            id="isFavorite"
+            type="checkbox"
+            checked={isFavorite}
+            onChange={handleOnChange}
+          />
+          <label htmlFor="isFavorite" className="form__group__input-label">
+            Add to favorites
+          </label>
+        </div>
+        <div className="form__group form__group--horizontal">
+          <input
+            className="form__group__input"
+            id="isDangerous"
+            type="checkbox"
+            checked={isDangerous}
+            onChange={handleOnChange}
+          />
+          <label htmlFor="isDangerous" className="form__group__input-label">
+            Is it dangerous?
+          </label>
+        </div>
+      </div>
+      <button className="submit-button" type="submit" disabled={!isValidData}>
+        Send report
+      </button>
+    </form>
+  );
+};
+
+export default BugForm;
