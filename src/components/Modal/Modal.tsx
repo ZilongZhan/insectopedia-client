7cecb2f6b49aeabf445544cd4dbc721e03447072
Zilong Zhan <zolong.zhan@gmail.com>
2025-05-27 19:03:35 +0200

    Add Modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import type React from "react";
+import useApp from "../../hooks/useApp";
+import Button from "../Button/Button";
+import DeleteButtonSvg from "../shared/DeleteButtonSvg/DeleteButtonSvg";
+import ErrorSvg from "../shared/ErrorSvg/ErrorSvg";
+
+import "./Modal.css";
+
+interface ModalProps {
+  message: string;
+  isErrorModal?: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  message,
+  isErrorModal: error = false,
+}) => {
+  const { modalConfig, setModalConfig } = useApp();
+  const modifier = error ? " modal__message--error" : "";
+
+  const handleCloseModal = (): void => {
+    setModalConfig({ ...modalConfig, showModal: false });
+  };
+
+  return (
+    <dialog open className="modal">
+      <Button onClick={handleCloseModal} modifier="modal-backdrop" />
+      <div className="modal__content">
+        <Button
+          modifier="delete"
+          aria-label="Close modal"
+          onClick={handleCloseModal}
+        >
+          <DeleteButtonSvg aria-hidden={true} />
+        </Button>
+        <span className={`modal__message${modifier}`}>{message}</span>
+        {error && <ErrorSvg />}
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
